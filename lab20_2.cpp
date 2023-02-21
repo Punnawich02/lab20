68ecaa2d5e0a8207bec990228d797ff6b21ec6c3
Punnawich Mai <punnawich_dachin@gmail.com>
2023-02-21 15:41:13 +0700

    Finished

--- lab20_2.cpp
+++ lab20_2.cpp
@@ -5,8 +5,11 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *R1,Rect *R2)
+{
+	double L = min(R1 -> x + R1 -> w,R2 -> y + R2 -> w) - max(R1 -> x,R2 -> x);
+	double T = min(R1 -> y,R2 -> y) - max(R1 -> y - R1 -> h ,R2 -> y - R2 -> h);
+	if(L < 0 or T < 0) return 0;
+	else return L*T;
 }
+
